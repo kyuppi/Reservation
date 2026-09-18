7d0d7d61e23be58da80cddbf1a9f44f097e45ff7
kyuppi <izuyuto771077@gmail.com>
2026-09-18 13:40:17 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// 初回セットアップ時に、Google Apps Script の「ウェブアプリURL」だけを貼り付けてください。
+// 例: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.APP_CONFIG = {
+  GAS_WEB_APP_URL: 'PASTE_YOUR_GAS_WEB_APP_URL_HERE'
+};

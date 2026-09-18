fb0c0820f0a7479d5cb628a6f6ee14b3106ad097
kyuppi <izuyuto771077@gmail.com>
2026-09-18 13:22:47 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// 初回セットアップ時に、Google Apps Script の「ウェブアプリURL」だけを貼り付けてください。
-// 例: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-window.APP_CONFIG = {
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxfYRMjOf5gYlSVlPAiukR8x4lwLEClm3bDL0dhyPlBqsoa8BIA6dJfsyCEEyFgLPY/exec'
-};

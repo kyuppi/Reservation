ccc4be0dfed3ec4e2fb8d7ad619f0c226c2c364d
kyuppi <izuyuto771077@gmail.com>
2026-09-18 13:31:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 初回セットアップ時に、Google Apps Script の「ウェブアプリURL」だけを貼り付けてください。
 // 例: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
 window.APP_CONFIG = {
-  GAS_WEB_APP_URL: 'PASTE_YOUR_GAS_WEB_APP_URL_HERE'
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx1xZgMwcZu-yTdQMlm0IhoRIEM-vc9yBDuKKlV9bb56rTkHtcrBh2iJnf40CxcLBBJ/exec'
 };
